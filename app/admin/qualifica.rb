27d8d1f736d899cf34c16e925ea17fcd97608547
Prelang Builder <builder@prelang.com>
2015-08-07 10:51:02 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Qualifica' to allow for create, update

--- app/admin/qualifica.rb
+++ app/admin/qualifica.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Qualifica do
+  permit_params :qualifica
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
